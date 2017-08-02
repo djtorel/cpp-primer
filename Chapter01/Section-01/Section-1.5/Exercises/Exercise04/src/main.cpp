942ccd62de945ba73fd806128355e498d19250c9
Dominic Torruellas <dtorruellas@gmail.com>
2017-08-01 20:18:59 -0700

    Section 1.5, Exercise 04

---
+++ Chapter01/Section-01/Section-1.5/Exercises/Exercise04/src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * Write programs that contain the common errors discussed on page 16.
+ * Familiarize yourself with the messages.
+ */
+
+// error: missing ) in parameter list for mainint main ( {
+// error: used colon, not a semicolon, after endl    std::cout << "Read each file." << std::endl:
+// error: missing quotes around string literal    std::cout << Update master. << std::endl;
+// error: second output operator is missing    std::cout << "Write new master." std::endl;
+// error: missing ; on return statement    return 0}
+
+#include <iostream>
+
+int main() {
+  // std::cout << "Read each file." << std::endl:
+  // std::cout << Update master. << std::endl;
+  // std::cout << "Write new master." std::endl;
+
+  return 0
+}
