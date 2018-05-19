6123fd190a5f056655754c791eee359fd0a141be
Dominic Torruellas <dtorruellas@gmail.com>
2018-05-18 21:15:22 -0700

    decided to add formatting... becaus reasons

--- Chapter03/Section-3.3.2/section-exercises/exercise-3.14/src/main.cpp
+++ Chapter03/Section-3.3.2/section-exercises/exercise-3.14/src/main.cpp
@@ -8,6 +8,30 @@
 #include <string>
 #include <vector>
 
+std::string vec_to_string(std::vector<int> vec) {
+    // Format the vec to a printable format and store in string
+    // Beginning of formatted string
+    std::string out_string{"[\n  "};
+    int cnt{0};
+    // Loop through vec, increase cnt or add a new line and space, and reset cnt
+    for (auto num : vec) {
+        if (cnt < 5) {
+            cnt++;
+        } else {
+            out_string += "\n  ";
+            cnt = 1;
+        }
+        // Add the num to the string with tab spacing
+        out_string += std::to_string(num) + ",\t";
+    }
+
+    // Remove the final comma and add new line with end bracket
+    out_string = out_string.substr(0, out_string.length() - 2);
+    out_string += "\n]";
+
+    return out_string;
+}
+
 int main() {
     std::vector<int> int_store{};
     std::string input{};
@@ -20,6 +44,7 @@ int main() {
         if (input == "/q") {
             break;
         }
+        // Check if it's a valid int
         bool is_valid{true};
         for (auto ch : input) {
             if (!std::isdigit(ch)) {
@@ -27,6 +52,7 @@ int main() {
             }
         }
 
+        // Store if valid, return error if not
         if (is_valid) {
             int_store.push_back(std::stoi(input));
         } else {
@@ -36,16 +62,9 @@ int main() {
 
     std::cout << "The numbers stored are:" << std::endl;
 
-    int cnt{0};
-    for (auto num : int_store) {
-        if (cnt < 5) {
-            cnt++;
-        } else {
-            std::cout << std::endl;
-            cnt = 1;
-        }
-        std::cout << num << " ";
-    }
+    std::string output = vec_to_string(int_store);
+
+    std::cout << output << std::endl;
 
     return 0;
 }
