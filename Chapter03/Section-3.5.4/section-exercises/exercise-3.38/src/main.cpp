5f164a34ea8d335b528dcc604806810c9e44de24
Dominic Torruellas <dtorruellas@gmail.com>
2018-05-21 11:33:44 -0700

    section 3.5.4 - exercise 3.38

---
+++ Chapter03/Section-3.5.4/section-exercises/exercise-3.38/src/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * Exercise 3.38: In this section, we noted that it was not only illegal but
+ * meaningless to try to add two pointers. Why would adding two pointers be
+ * meaningless?
+ *
+ * A: A pointer is an address in memory. Adding two addresses would have almost
+ * no use, as there isn't a real scenerio where that would return a predictable
+ * address that could be used.
+ */
